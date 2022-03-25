6245d1f42bb4d49289edde371277635e20fa3895
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2022-03-25 17:52:53 +0000

    Rename dist->docs

--- build.cmd
+++ build.cmd
@@ -3,7 +3,7 @@
 pushd MyWebApp
 dotnet build
 popd
-cp MyWebApp\bin\Debug\net7.0\MyWebApp.wasm dist\
-wasm-opt dist\MyWebApp.wasm -O3 -o dist\MyWebApp.opt.wasm
-brotli -f dist\MyWebApp.opt.wasm
-rm dist\*.wasm
+cp MyWebApp\bin\Debug\net7.0\MyWebApp.wasm docs\
+wasm-opt docs\MyWebApp.wasm -O3 -o docs\MyWebApp.opt.wasm
+brotli -f docs\MyWebApp.opt.wasm
+rm docs\*.wasm
